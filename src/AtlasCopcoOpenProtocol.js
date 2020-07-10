0f5af0c5d8a4f577f1d442be42f908ba69653458
Mariano S <mariano.simone@cybus.io>
2020-07-10 21:38:50 +0200

    openProtocol lib integration

--- src/AtlasCopcoOpenProtocol.js
+++ src/AtlasCopcoOpenProtocol.js
@@ -1,20 +1,52 @@
 const { EventEmitter } = require('events')
+const openProtocol = require('node-open-protocol')
 
 class AtlasCopcoOpenProtocol extends EventEmitter {
-  constructor (options) {
+  constructor (params) {
     super()
+
+    const defaultOptions = {
+      linkLayerActivate: undefined,
+      genericMode: undefined,
+      keepAlive: 10000,
+      rawData: false,
+      timeOut: 3000,
+      retryTimes: 3,
+      disableMidParsing: {}
+    }
+
+    this._connection = {
+      host: params.host,
+      port: params.port
+    }
+
+    this._options = Object.assign(defaultOptions, params.options)
   }
 
   async connect () {
+    await new Promise(resolve => {
+      this._client = openProtocol.createClient(this._connection.host,
+        this._connection.port, this._options, (data) => {
+          resolve(data)
+        })
+    })
+    this.emit('connected')
   }
 
   async disconnect () {
+    this._client.close()
+    this._client = undefined
+    this.emit('disconnected')
   }
 
-  async subscribe (address, callback, interval) {
+  async subscribe (address, callback) {
+    this._client.on(address.name, callback)
+    return this._client.subscribe(address.name)
   }
 
   async unsubscribe (address) {
+    this._client.removeAllListeners(address.name)
+    return this._client.unsubscribe(address.name)
   }
 
   async read (address) {
